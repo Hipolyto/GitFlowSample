d1c04a1527d4e21ec342f59a8363162d96c5c14f
Hipolyto <polo@letsolutions.com.mx>
2018-02-03 10:18:09 -0700

    changing name

--- GitFlowSample/GitFlowSample/App.xaml.cs
+++ GitFlowSample/GitFlowSample/App.xaml.cs
@@ -8,7 +8,7 @@ namespace GitFlowSample
     {
         public static bool UseMockDataStore = true;
         public static string BackendUrl = "https://localhost:5000";
-        private string something = "";
+        private string Something = "";
         
         public App()
         {
